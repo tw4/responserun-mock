9b5f3f065eca537a516aecac2cc581d3e59dc4a1
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-12 11:39:44 +0300

    add test

---
+++ test/createAddress.test.ts
@@ -0,0 +1,10 @@
+import { describe, test, expect } from 'vitest';
+import { createAddress } from '../src/random/address';
+import { Locale } from '../src/enums/locale';
+import { addresses } from '../src/data/locale/tr/address';
+
+describe('createAddress()', () => {
+  test('should return a address in Turkish', () => {
+    expect(addresses).toContain(createAddress(Locale.tr));
+  });
+});
