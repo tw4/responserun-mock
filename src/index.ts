0199155e7a0d58fa3062696f7fd46deb72f1028e
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-12 13:55:05 +0300

    feat uuid4

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './enums/locale';
 export * from './random/firstname';
 export * from './random/surname';
 export * from './random/address';
+export * from './random/uuid';
---
+++ src/random/uuid.ts
@@ -0,0 +1,20 @@
+export function generateUuid4() {
+  const hexChars = '0123456789abcdef';
+  let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
+  for (let i = 0; i < 36; i++) {
+    const placeholder = uuid[i];
+    if (placeholder === 'x') {
+      const randomIndex = Math.floor(Math.random() * 16);
+      uuid = uuid.replace('x', hexChars[randomIndex]);
+    } else if (placeholder === 'y') {
+      const randomIndex = Math.floor(Math.random() * 4) + 8; // 8, 9, A, B
+      uuid = uuid.replace('y', hexChars[randomIndex]);
+    }
+  }
+
+  return uuid;
+}
+
+const myUuid = generateUuid4();
+console.log(myUuid);
