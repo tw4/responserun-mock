3735ee219bcbe185770187471df0e60d96daa303
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-12 10:51:51 +0300

    fix: typo

--- test/createMaleFirstName.test.ts
+++ test/createMaleFirstName.test.ts
@@ -1,11 +1,11 @@
 import { createMaleFirstName } from '../src/random/firstname';
 import { Locale } from '../src/enums/locale';
 import { expect, test, describe } from 'vitest';
-import { maleFirstName } from '../src/data/locale/tr/maleFirstName';
+import { maleFirstNames } from '../src/data/locale/tr/maleFirstName';
 
 describe('createMaleFirstName()', () => {
   test('should return a male first name in Turkish', () => {
     const firstName = createMaleFirstName(Locale.tr);
-    expect(maleFirstName).toContain(firstName);
+    expect(maleFirstNames).toContain(firstName);
   });
 });
