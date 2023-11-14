117c1074de2afb56330b66d8caac3ae48ba84cea
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-14 15:56:47 +0300

    feat multiple randomizer

--- src/random/randomizer.ts
+++ src/random/randomizer.ts
@@ -2,3 +2,12 @@
 export function singleRandomizer(list: string[]) {
   return list[Math.floor(Math.random() * list.length)];
 }
+
+// This function returns a list filled with the given objects
+export function multipleRandomizer(fn: () => object, count: number) {
+  const result: any[] = [];
+  for (let i = 0; i < count; i++) {
+    result.push(fn());
+  }
+  return result;
+}
