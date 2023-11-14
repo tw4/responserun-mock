0f5b58726a1fb8ba7f8d7b1630ca717923ccbc37
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-14 15:58:08 +0300

    remove unused import

--- test/randomizer.test.ts
+++ test/randomizer.test.ts
@@ -1,8 +1,6 @@
 import { describe, test, expect } from 'vitest';
-import { singleRandomizer, multipleRandomizer } from '../src/random/randomizer';
+import { singleRandomizer } from '../src/random/randomizer';
 import { maleFirstNames } from '../src/data/locale/tr/maleFirstName';
-import { createMaleFirstName } from '../src/random/firstname';
-import { Locale } from '../src/enums/locale';
 
 describe('randomizer()', () => {
   test('singleRandomizer', () => {
