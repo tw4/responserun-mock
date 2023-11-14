5ad829600e638989c8e16120cbf6b71e35ce360d
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-14 15:56:58 +0300

    add test multiple randomizer

--- test/randomizer.test.ts
+++ test/randomizer.test.ts
@@ -1,6 +1,8 @@
 import { describe, test, expect } from 'vitest';
-import { singleRandomizer } from '../src/random/randomizer';
+import { singleRandomizer, multipleRandomizer } from '../src/random/randomizer';
 import { maleFirstNames } from '../src/data/locale/tr/maleFirstName';
+import { createMaleFirstName } from '../src/random/firstname';
+import { Locale } from '../src/enums/locale';
 
 describe('randomizer()', () => {
   test('singleRandomizer', () => {
